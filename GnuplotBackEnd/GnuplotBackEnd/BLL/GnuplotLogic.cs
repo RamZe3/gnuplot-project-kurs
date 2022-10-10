1576b77ce8674f48db93014941c3747acc137fd1
Ramil Fitkulin <ramil_fitkulin@epam.com>
2022-10-10 04:03:58 +0400

    init vue

---
+++ GnuplotBackEnd/GnuplotBackEnd/BLL/GnuplotLogic.cs
@@ -0,0 +1,14 @@
+﻿using GnuplotBackEnd.Entities;
+
+namespace GnuplotBackEnd.BLL
+{
+    public class GnuplotLogic
+    {
+        public GnuplotTemplate GnuplotTemplate { get; set; }
+
+        public bool Plot()
+        {
+            return true;
+        }
+    }
+}
---
+++ GnuplotBackEnd/GnuplotBackEnd/Entities/GnuplotTemplate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GnuplotBackEnd.Entities
+{
+    public class GnuplotTemplate
+    {
+        public String Name { get; set; }
+        public String Function { get; set; }
+        public int XRange { get; set; }
+        public int YRange { get; set; }
+        public bool Grid { get; set; }
+        public String Background { get; set; }
+        
+    }
+}
