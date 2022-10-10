9d2219b0293e0300e84a7cb6f602ad3a0ba662d8
Ramil Fitkulin <ramil_fitkulin@epam.com>
2022-10-11 02:53:34 +0400

    add base template of application

--- gnuplot-backend/src/App.vue
+++ gnuplot-backend/src/App.vue
@@ -1,37 +1,50 @@
 <template>
-<nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <router-link class="nav-link active" aria-current="page" to="/">Главная</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/about">Шаблоны</router-link>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+  <header class="header">
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Gnuplotter</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link active" aria-current="page" to="/">Главная</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/about">Шаблоны</router-link>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                 aria-expanded="false">
+                Профиль
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled">Disabled</a>
+            </li>
           </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <main class="content ">
+    <router-view/>
+  </main>
+  <footer class="footer">
+    <div class="footer-div d-flex flex-column  justify-content-between border-top">
+      <p>© 2022 Company, Inc. All rights reserved.</p>
     </div>
-  </div>
-</nav>
-<router-view/>
+  </footer>
 </template>
 
 <style>
@@ -41,6 +54,32 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  align-items: center;
+  justify-content: center;
+}
+
+.header{
+  min-width: 100%;
+}
+
+.footer-div{
+  padding-top: 1.5rem!important;
+  margin-top: 1.5rem!important;
+}
+
+.footer{
+  margin-top: auto;
+  min-width: 100%;
+}
+
+.content{
+  width: 60%;
+  justify-content: center;
 }
 
 nav {
--- gnuplot-backend/src/views/HomeView.vue
+++ gnuplot-backend/src/views/HomeView.vue
@@ -1,17 +1,12 @@
 <template>
+  <h2>Gnupot Result</h2>
+  <img src="../assets/testiii.png" class="img-fluid" alt="Logo">
+
+  <h2>Gnupot Settings</h2>
   <div class="spinner-border text-primary" role="status">
     <span class="sr-only"></span>
   </div>
-  <img src="../assets/logo.png" class="img-fluid" alt="Logo">
-
   <div class="row">
-    <div class="col">
-      <div class="input-group mb-3">
-        <span class="input-group-text" id="basic-addon1">Function</span>
-        <input type="text" class="form-control" placeholder="sin(x)" aria-label="Username"
-               aria-describedby="basic-addon1">
-      </div>
-    </div>
     <div class="col">
       <div class="input-group mb-3">
         <span class="input-group-text" id="basic-addon1">Function</span>
@@ -21,14 +16,37 @@
     </div>
     <div class="col">
       <div class="input-group">
-        <span class="input-group-text">First and last name</span>
+        <span class="input-group-text">Range</span>
         <input type="text" aria-label="First name" class="form-control">
         <input type="text" aria-label="Last name" class="form-control">
       </div>
     </div>
   </div>
   <div class="row">
-    <div class="col-8">
+    <div class="col">
+      <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">Function</span>
+        <input type="text" class="form-control" placeholder="sin(x)" aria-label="Username"
+               aria-describedby="basic-addon1">
+      </div>
+    </div>
+    <div class="col">
+      <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">Function</span>
+        <input type="text" class="form-control" placeholder="sin(x)" aria-label="Username"
+               aria-describedby="basic-addon1">
+      </div>
+    </div>
+    <div class="col">
+      <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">Function</span>
+        <input type="text" class="form-control" placeholder="sin(x)" aria-label="Username"
+               aria-describedby="basic-addon1">
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
       <div class="input-group mb-3">
         <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
                 aria-expanded="false">Dropdown
@@ -45,33 +63,48 @@
         <input type="text" class="form-control" aria-label="Text input with dropdown button">
       </div>
     </div>
-    <div class="col-4">col-4</div>
-  </div>
-  <div class="row">
-    <div class="input-group mb-3">
-      <label class="input-group-text" for="inputGroupSelect01">Options</label>
-      <select class="form-select" id="inputGroupSelect01">
-        <option selected>Choose...</option>
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
-      </select>
+    <div class="col">
+      <div class="input-group mb-3">
+        <label class="input-group-text" for="inputGroupSelect01">Options</label>
+        <select class="form-select" id="inputGroupSelect01">
+          <option selected>Choose...</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+      </div>
     </div>
   </div>
   <div class="row">
-    <div class="input-group">
-      <span class="input-group-text">With textarea</span>
-      <textarea class="form-control" aria-label="With textarea"></textarea>
-    </div>
-    <div class="input-group mb-3">
-      <label class="input-group-text" for="inputGroupFile01">Upload</label>
-      <input type="file" class="form-control" id="inputGroupFile01">
+    <div class="col">
+      <div class="input-group mb-3">
+        <label class="input-group-text" for="inputGroupFile01">Upload</label>
+        <input type="file" class="form-control" id="inputGroupFile01">
+      </div>
     </div>
   </div>
-  <form class="form-inline">
-    <button class="btn btn-outline-success" type="button">Main button</button>
-    <button class="btn btn-sm btn-outline-secondary" type="button">Smaller button</button>
-  </form>
+  <div class="row">
+    <div class="col col-area">
+      <div class="input-group">
+        <span class="input-group-text">With textarea</span>
+        <textarea class="form-control" aria-label="With textarea"></textarea>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col col-area">
+      <div class="input-group">
+        <span class="input-group-text">With textarea</span>
+        <textarea class="form-control" aria-label="With textarea"></textarea>
+      </div>
+    </div>
+  </div>
+  <div class="row buttons">
+    <button type="button" class="btn btn-secondary btn-lg btn-block">Plot Graphic</button>
+  </div>
+  <div class="row buttons">
+    <button type="button" class="btn btn-secondary btn-lg btn-block">Block level button</button>
+  </div>
 </template>
 
 <script>
@@ -82,3 +115,14 @@ export default {
   components: {}
 }
 </script>
+
+<style>
+ .col-area{
+   margin-top: 10px;
+   margin-bottom: 10px;
+ }
+
+ .buttons{
+   padding-top: 20px;
+ }
+</style>
